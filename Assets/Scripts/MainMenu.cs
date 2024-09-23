360bc0b82acb3e67251f3860e3f69bd7cbd44387
Thaís Ferreira <thferreirasa@gmail.com>
2024-09-23 13:02:45 -0300

    adição do menu principal

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Principal");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Para parar no editor
+        #endif
+    }
+}
